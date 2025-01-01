8d950828334c71f945b049f6e503e59f74d9a827
Tom Eugelink <tbee@tbee.org>
2025-01-01 08:32:07 +0100

    v1.1.0

--- mvn_release.cmd
+++ mvn_release.cmd
@@ -1,3 +1,6 @@
+if exist javaHome.cmd (
+    call javaHome.cmd
+)
 rem call mvnw jreleaser:config
 call mvnw clean deploy jreleaser:full-release -DaltDeploymentRepository=local::file:./target/staging-deploy
 pause
