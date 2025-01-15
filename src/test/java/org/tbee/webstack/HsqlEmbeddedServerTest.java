586164426e0ebb9a0570fcedf6166e8a2d73e2b2
Tom Eugelink <tbee@tbee.org>
2025-01-15 03:04:00 +0100

    rename to HsqlEmbeddedServer to differentiate between in memory

--- src/test/java/org/tbee/webstack/HsqlEmbeddedServerTest.java
+++ src/test/java/org/tbee/webstack/HsqlEmbeddedServerTest.java
@@ -4,7 +4,7 @@ import org.apache.commons.io.FileUtils;
 import org.jdbi.v3.core.Jdbi;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.tbee.webstack.hsqldb.HsqlEmbedded;
+import org.tbee.webstack.hsqldb.HsqlEmbeddedServer;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,7 +14,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
-public class HsqlEmbeddedTest {
+public class HsqlEmbeddedServerTest {
 
     private int port = -1;
 
@@ -33,7 +33,7 @@ public class HsqlEmbeddedTest {
     public void startUseStop() {
 
         // WHEN hsql is started
-        HsqlEmbedded hsqlEmbedded = new HsqlEmbedded()
+        HsqlEmbeddedServer hsqlEmbeddedServer = new HsqlEmbeddedServer()
                 .port(port)
                 .username("username")
                 .password("password")
@@ -48,11 +48,34 @@ public class HsqlEmbeddedTest {
             return handle.createQuery("SELECT name FROM user").mapTo(String.class).list();
         });
         assertEquals(1, names.size());
-        assertEquals("Alice", names.get(0));
+        assertEquals("Alice", names.getFirst());
 
         // AND shutdown
-        hsqlEmbedded.stop();
+        hsqlEmbeddedServer.stop();
+
+        // THEN
+    }
+
+    @Test
+    public void portFromUrl() {
+
+        // WHEN hsql configured
+        HsqlEmbeddedServer hsqlEmbeddedServer = new HsqlEmbeddedServer()
+                .portFromUrl("jdbc:hsqldb:hsql://localhost:" + port + "/" + this.getClass().getSimpleName());
 
         // THEN all should have works without issues
+        assertEquals(port, hsqlEmbeddedServer.port());
+    }
+
+    @Test
+    public void databaseFromUrl() {
+
+        // WHEN hsql configured
+        HsqlEmbeddedServer hsqlEmbeddedServer = new HsqlEmbeddedServer()
+                .databaseFromUrl("jdbc:hsqldb:hsql://localhost:" + port + "/" + this.getClass().getSimpleName());
+
+        // THEN
+        assertEquals(1, hsqlEmbeddedServer.databases().size());
+        assertEquals(this.getClass().getSimpleName(), hsqlEmbeddedServer.databases().getFirst());
     }
 }
