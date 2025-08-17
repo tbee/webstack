fc2fad48881179101de3545d42153918d26fef47
Tom Eugelink <tbee@tbee.org>
2025-08-17 08:42:32 +0200

    added linux scripts

---
+++ javaHome.sh
@@ -0,0 +1,3 @@
+export SDKMAN_DIR="$HOME/.sdkman"
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk use java 23.0.2-zulu
---
+++ mvn_cleanInstall.sh
@@ -0,0 +1,5 @@
+if [ -f javaHome.sh ]; then
+   javaHome.sh
+fi
+
+mvnw clean install
